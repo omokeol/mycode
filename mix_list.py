a26037193e33925a36478ee896ce5b7dc868acc0
Olamide Omokeji <olamide.omokeji@verizonwireless.com>
2018-12-11 14:40:13 -0500

    Lab 10

---
+++ mix_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# This declares what is in my list
+my_list = ["192.168.0.5', 5060, "UP"] 
+
+# This displays the first item in my list
+print("The first item in the list (IP): " + my_list[0] )
+
+# This displays the second item in my list
+print("The second item in the list (port): " + str(my_list[1]) )
+
+# This displays the third item in my list
+print("The last item in the list (state): " + my_list[2] )
