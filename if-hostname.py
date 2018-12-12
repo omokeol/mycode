e4382b8f446ad8113f6930aaf5c46f557a9eb3d6
Olamide Omokeji <olamide.omokeji@verizonwireless.com>
2018-12-12 15:37:14 -0500

    lab 13

---
+++ if-hostname.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#Now create a string hostname that we can test against.
+hostname = 'MTG'
+if hostname = 'MTG':
+
+#Now what to do if this statement is found to be true, don’t forget to indent!
+print('The hostname was found to be mtg')
+
+
---
+++ if-test/if-hostname.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+#Now create a string hostname that we can test against.
+
+#hostname = 'MTG'
+#if hostname == 'MTG':
+
+#Now what to do if this statement is found to be true, don’t forget to indent!
+#   print('The hostname was found to be mtg')
+
+#######################################################################
+#1st Code Customization Request - Make hostname defined by collecting input from the user. Include a nice question asking for a hostname.
+
+#hostname = input("Please enter your hostname:")
+#print("your hostname is:", hostname)
+########################################################################
+#2nd Code Customization Request - Ensure that the user can enter mtg, MTG, mTg, MTg, mTG, or MtG, and they would all test true.
+
+#hostname = input("Please enter your hostname:")
+#if hostname.upper() == "MTG":
+#hostname.lower() = 'mtg'
+#	print("your hostname is:", hostname)
+##########################################################################
+
+#3rd Code Customization Request - If hostname test true, print a second line that says hostname matches expected config to the screen.
+#hostname = input("Please enter your hostname:")
+#if hostname.upper() == "MTG":
+#	print("hostname matches expected config to the screen")
+##########################################################################
+#4th Code Customization Request - Before the program ends, always print Exiting the script.
+#hostname = input("Please enter your hostname:")
+#if hostname.upper() == "MTG":
+#	print("hostname matches expected config to the screen")
+#	print('Exiting the script')
+
+##############################################################################
+
+#5th Code Customization Request - Comment all the code
+
+
+
+
+
+
